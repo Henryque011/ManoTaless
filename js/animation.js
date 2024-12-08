263d4a57de823457fe56d73cd843bbc8064d725b
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2024-12-07 23:17:20 -0300

    Pull - js

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,10 @@
-document.querySelector(".abrirMenu").onclick = function () {
-    document.documentElement.classList.add("menuAtivo");
-}
-document.querySelector(".fecharMenu").onclick = function() {
-    document.documentElement.classList.remove("menuAtivo");
-}
+
+// Seleciona o botão do menu e a navegação
+const menuButton = document.querySelector('.menu');
+const navMenu = document.querySelector('nav');
+
+// Adiciona um evento de clique ao botão
+menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('active'); // Alterna a classe 'active'
+});
+
