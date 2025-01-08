124007cecfa532e54c76b2512de998fa7eabe233
og <henryquenonatosilva@gmail.com>
2025-01-08 15:49:03 -0300

    animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,9 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const menuToggle = document.getElementById("menu-toggle");
-//     const menuClose = document.getElementById("menu-close");
-//     const menuLateral = document.getElementById("menu-lateral");
+// carrossel de trabalhos
 
-//     // Abrir o menu
-//     menuToggle.addEventListener("click", () => {
-//         menuLateral.classList.add("active");
-//     });
-
-//     // Fechar o menu
-//     menuClose.addEventListener("click", () => {
-//         menuLateral.classList.remove("active");
-//     });
-// });
+$(document).ready(function () {
+    $('.exe_trabalho').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
