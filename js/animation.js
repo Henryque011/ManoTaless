fef91f198e389a4c9cd0ad1234cdea0b6334eecc
og <henryquenonatosilva@gmail.com>
2025-01-09 15:55:13 -0300

    animation.js

--- js/animation.js
+++ js/animation.js
@@ -2,8 +2,9 @@
 $(document).ready(function () {
     $('.box_carousel').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 2000
     });
 });
