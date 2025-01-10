f06b2827887f1652fc267c38f4c18b9bc7e852ab
og <henryquenonatosilva@gmail.com>
2025-01-10 20:44:15 -0300

    animation.js

--- js/animation.js
+++ js/animation.js
@@ -9,3 +9,14 @@ $(document).ready(function () {
         dots: true
     });
 });
+
+$(document).ready(function () {
+    $('.box_video').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
+    });
+});
