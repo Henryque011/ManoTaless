9fad061d2a72fecb7c2f4ba048bab4085d416ce4
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-01-11 02:17:07 -0300

    animation.js

--- js/animation.js
+++ js/animation.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.box_video').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         dots: true
