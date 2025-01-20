b975c6c15dc1def98ecfc21bde996bdc3d6dd11f
og <henryquenonatosilva@gmail.com>
2025-01-20 15:51:43 -0300

    script

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// botão upper 
+const btnScrollTop = document.querySelector(".btn-scroll-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+    btnScrollTop.classList.add("show-btn-scroll-top");
+    } else {
+    btnScrollTop.classList.remove("show-btn-scroll-top");
+    }
+});
+
+btnScrollTop.addEventListener("click", () => {
+    window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+    });
+});
+
+// Menu
+const menuToggle = document.getElementById('menu-toggle');
+const menuLateral = document.getElementById('menu-lateral');
+const body = document.querySelector('body');
+const btnSair = document.querySelector('.btn-sair');  
+
+menuToggle.addEventListener('click', () => {
+    menuLateral.classList.toggle('ativo');
+    body.classList.toggle('body-ativo');
+});
+
+btnSair.addEventListener('click', () => {
+    menuLateral.classList.remove('ativo');  
+    body.classList.remove('body-ativo');  
+});
