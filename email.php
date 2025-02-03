07c478b9ddac811cc68c836ff592e861f6a0879c
og <henryquenonatosilva@gmail.com>
2025-02-03 15:56:59 -0300

    email.php

--- email.php
+++ email.php
@@ -1,7 +1,10 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 require_once("vendors/PHPMailer/src/PHPMailer.php");
 require_once("vendors/PHPMailer/src/SMTP.php");
+$ok =0;
 
 try {
     if (isset($_POST["email"])) {
@@ -11,30 +14,30 @@ try {
         $mensagem       = $_POST["mensagem"];
         $assunto     = "CONTATO VIA SITE";
 
-        $phpmail = new PHPMailer\PHPMailer();
-        $phpmail->isSMTP();
-        $phpmail->SMTPDebug = 0;
+        $phpmail = new PHPMailer\PHPMailer\PHPMailer();
+        $phpmail-> isSMTP();
+        $phpmail-> SMTPDebug = 0;
 
-        $phpmail->Host = "smtp.hostinger.com";
-        $phpmail->port = 465;
+        $phpmail-> Host = "smtp.hostinger.com";
+        $phpmail-> port = 465;
 
-        $phpmail->STMPSecure = 'ssl';
-        $phpmail->SMTPAuth = true;
+        $phpmail-> STMPSecure = 'ssl';
+        $phpmail-> SMTPAuth = true;
 
-        $phpmail->Username = "";
-        $phpmail->Password = "";
-        $phpmail->IsHTML(true);
-        $phpmail->setFrom("", $nome); //remetente
-        $phpmail->addAddress("", $assunto);
+        $phpmail-> Username = "tipitwo@tipi02.smpsistema.com.br";
+        $phpmail-> Password = "Senac@tipitwo01";
+        $phpmail-> IsHTML(true);
+        $phpmail-> setFrom("abxqtzseven@gmail.com", $nome); //remetente
+        $phpmail-> addAddress("abxqtzseven@gmail.com", $assunto); //destinatario
 
-        $phpmail->Subject = $assunto;
+        $phpmail-> Subject = $assunto;
 
-        $phpmail->msgHTML("Nome: $nome <br>
+        $phpmail-> msgHTML("Nome: $nome <br>
                             E-Mail: $email <br>
                             Telefone: $telefone <br>
                             Mensagem: $mensagem");
 
-        $phpmail->AltBody = "Nome: $nome \n
+        $phpmail-> AltBody = "Nome: $nome \n
                             E-Mail: $email \n
                             Telefone: $telefone \n
                             Mensagem: $mensagem";
@@ -44,7 +47,7 @@ try {
             require_once("index.html");
         } else {
             $ok = 2;
-            echo "Não foi possivel enviar a mensagem. Erro: " . $phpmail->ErrorInfo;
+            echo "Não foi possivel enviar a mensagem. Erro: " .$phpmail->ErrorInfo;
         }
     }
 } catch (Execption $e) {
