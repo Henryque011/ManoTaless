af1daf216c5f6a9ca2f208c83c28287f522eedfb
og <henryquenonatosilva@gmail.com>
2025-02-03 15:41:45 -0300

    email.php

--- email.php
+++ email.php
@@ -3,6 +3,50 @@
 require_once("vendors/PHPMailer/src/PHPMailer.php");
 require_once("vendors/PHPMailer/src/SMTP.php");
 
-try{
-    
+try {
+    if (isset($_POST["email"])) {
+        $nome           = $_POST["nome"];
+        $telefone       = $_POST["tel"];
+        $email          = $_POST["email"];
+        $mensagem       = $_POST["mensagem"];
+        $assunto     = "CONTATO VIA SITE";
+
+        $phpmail = new PHPMailer\PHPMailer();
+        $phpmail->isSMTP();
+        $phpmail->SMTPDebug = 0;
+
+        $phpmail->Host = "smtp.hostinger.com";
+        $phpmail->port = 465;
+
+        $phpmail->STMPSecure = 'ssl';
+        $phpmail->SMTPAuth = true;
+
+        $phpmail->Username = "";
+        $phpmail->Password = "";
+        $phpmail->IsHTML(true);
+        $phpmail->setFrom("", $nome); //remetente
+        $phpmail->addAddress("", $assunto);
+
+        $phpmail->Subject = $assunto;
+
+        $phpmail->msgHTML("Nome: $nome <br>
+                            E-Mail: $email <br>
+                            Telefone: $telefone <br>
+                            Mensagem: $mensagem");
+
+        $phpmail->AltBody = "Nome: $nome \n
+                            E-Mail: $email \n
+                            Telefone: $telefone \n
+                            Mensagem: $mensagem";
+        if ($phpmail->send()) {
+            $ok = 1;
+            echo "Mensagem enviada com sucesso!";
+            require_once("index.html");
+        } else {
+            $ok = 2;
+            echo "Não foi possivel enviar a mensagem. Erro: " . $phpmail->ErrorInfo;
+        }
+    }
+} catch (Execption $e) {
 }
+?>
