1af75b1eedc02c38277f4dbdf68b4ec13cf94b03
og <henryquenonatosilva@gmail.com>
2025-02-07 15:57:24 -0300

    ntor.php

---
+++ template/pg_servico/cantor.php
@@ -0,0 +1,13 @@
+<section class="cantor">
+    <article class="site">
+        <h2>Acompanhe meu trablaho artistico</h2>
+        <div class="redes_sociais">
+            <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
+                    class="fa-brands fa-instagram"></i></a>
+            <a href="https://wa.me/5511970428582" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank"><i
+                    class="fa-brands fa-tiktok"></i></a>
+            <a href="#"><i class="fa-brands fa-spotify"></i></a>
+        </div>
+    </article>
+</section>
