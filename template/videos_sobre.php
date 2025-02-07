5dcd5c077133d589418dc10897f58983625ee698
og <henryquenonatosilva@gmail.com>
2025-02-07 15:22:13 -0300

    videos.php

---
+++ template/videos_sobre.php
@@ -0,0 +1,48 @@
+<section class="videos_sobre">
+    <div class="banner">
+    </div>
+    <div class="space">
+
+    </div>
+    <article class="site">
+        <div class="side">
+            <div>
+                <h2>veja alguns vídeos do meu trabalho</h2>
+                <h3>siga minhas redes socias</h3>
+                <div class="redes_sociais">
+                    <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
+                            class="fa-brands fa-instagram"></i></a>
+                    <a href="https://wa.me/5511970428582" target="_blank"><i
+                            class="fa-brands fa-whatsapp"></i></a>
+                    <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank"><i
+                            class="fa-brands fa-tiktok"></i></a>
+                    <a href="#"><i class="fa-brands fa-spotify"></i></a>
+                </div>
+                <h3>todo contato é feito exclusivamente pelo <span style="color: #7d1ecc;">whatsapp</span></h3>
+                <button><a href="contato.html">entre em contato</a></button>
+            </div>
+            <div class="box_video">
+                <div><video width="400px" controls>
+                        <source src="video/video1.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div><video width="400px" controls>
+                        <source src="video/video1.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div><video width="400px" controls>
+                        <source src="video/video1.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div><video width="400px" controls>
+                        <source src="video/video1.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div><video width="400px" controls>
+                        <source src="video/video1.mp4" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
