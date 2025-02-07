536797391185d9f468a674763f38e81f5c3e1966
og <henryquenonatosilva@gmail.com>
2025-02-07 15:57:16 -0300

    cabelos.php

---
+++ template/pg_servico/cabelos.php
@@ -0,0 +1,45 @@
+<section class="cabelos">
+    <article class="site">
+        <h2>Meus trabalhos</h2>
+        <h3>trançista - barbeiro - dreadmaker</h3>
+        <div class="side">
+            <div class="box_cabelo">
+                <div>
+                    <img src="img/corte1.png" alt="Corte de cabelo">
+                    <p>cortes a patir de 30 reais</p>
+                </div>
+                <div>
+                    <img src="img/sobrancelha1.jpeg" alt="sobrancelha">
+                    <p>sobrancelha a patir de 15 reais</p>
+                </div>
+                <div>
+                    <img src="img/dread1.png" alt="Dreads">
+                    <p>dreads a patir de 300 reais</p>
+                </div>
+            </div>
+            <div class="box_cabelo">
+                <div>
+                    <p>tranças a patir de 100 reais</p>
+                    <img src="img/tranca1.png" alt="Tranças">
+                </div>
+                <div>
+                    <p>barba a patir de 15 reais</p>
+                    <img src="img/barba1.jpeg" alt="Barbas">
+                </div>
+                <div>
+                    <p>cortes apatir de R$30</p>
+                    <img src="img/corte3.png" alt="Cortes de cabelos">
+                </div>
+            </div>
+        </div>
+        <h4>para marcar seu horario entre em contato diretamente pelo <span style="color: #7d1ecc;"> whatsapp
+            </span></h4>
+        <div class="cabelos_links">
+            <div class="side">
+                <button><a href="https://wa.me/5511970428582" target="_blank">whatsapp</a></button>
+                <button><a href="contato.html">redes sociais</a></button>
+            </div>
+        </div>
+        <div class="space"></div>
+    </article>
+</section>
