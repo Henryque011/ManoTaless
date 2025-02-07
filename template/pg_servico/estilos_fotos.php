f9ad15f0a592dfd4b9fe2eeff6bb7367f321f507
og <henryquenonatosilva@gmail.com>
2025-02-07 15:57:43 -0300

    stilos.fotos.php

---
+++ template/pg_servico/estilos_fotos.php
@@ -0,0 +1,136 @@
+<section class="estilos_fotos">
+    <article class="site">
+        <h2>veja foto dos <span style="color: #7d1ecc;">estilos</span></h2>
+        <div class="box_estilos">
+            <div class="button_estilos">
+                <button id="todos">Todos</button>
+                <button id="cortes">Cortes</button>
+                <button id="trancas">Tranças</button>
+                <button id="barbas">Barbas</button>
+                <button id="dreads">Dreads</button>
+                <button id="sobrancelha">Sobrancelha</button>
+            </div>
+            <div class="imgs_box">
+                <div class="imagem_container">
+                    <div class="box_corte">
+                        <div class="imagemCorte-container">
+                            <img class="imagemCorte" src="img/corte1.png"
+                                alt="Corte masculino com fade lateral">
+                            <div class="texto-hover">Corte masculino com fade lateral</div>
+                        </div>
+                        <div class="imagemCorte-container">
+                            <img class="imagemCorte" src="img/corte2.png"
+                                alt="Corte de cabelo médio/grnade com fade lateral">
+                            <div class="texto-hover">Corte masculino com fade lateral</div>
+                        </div>
+                        <div class="imagemCorte-container">
+                            <img class="imagemCorte" src="img/corte3.png" alt="Corte midfade com risquinho">
+                            <div class="texto-hover">Corte masculino com fade lateral</div>
+                        </div>
+                        <div class="imagemCorte-container">
+                            <img class="imagemCorte" src="img/corte2.png"
+                                alt="Corte de cabelo médio/grnade com fade lateral">
+                            <div class="texto-hover">Corte masculino com fade lateral</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="imagem_container">
+                    <div class="box_tranca">
+                        <div class="imagemTranca-container">
+                            <img class="imagemTranca" src="img/tranca1.png" alt="Trança nagô">
+                            <div class="texto-hover">Trança nagô</div>
+                        </div>
+                        <div class="imagemTranca-container">
+                            <img class="imagemTranca" src="img/tranca2.png"
+                                alt="Trança box braid com comprimento longo">
+                            <div class="texto-hover">Trança box braid com comprimento longo</div>
+                        </div>
+                        <div class="imagemTranca-container">
+                            <img class="imagemTranca" src="img/tranca3.png" alt="Trança nagô simples">
+                            <div class="texto-hover">Trança nagô simples</div>
+                        </div>
+                        <div class="imagemTranca-container">
+                            <img class="imagemTranca" src="img/tranca4.png"
+                                alt="Trança box braid com comprimento longo e decoração">
+                            <div class="texto-hover">Trança box braid com comprimento longo e decoração</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="imagem_container">
+                    <div class="box_barba">
+                        <div class="imagemBarba-container">
+                            <img class="imagemBarba" src="img/barba1.jpeg"
+                                alt="Barba cheia com modelagem quadrada">
+                            <div class="texto-hover">Barba cheia com modelagem quadrada</div>
+                        </div>
+                        <div class="imagemBarba-container">
+                            <img class="imagemBarba" src="img/barba1.jpeg"
+                                alt="Barba curta e bem alinhada com cavanhaque">
+                            <div class="texto-hover">Barba curta e bem alinhada com cavanhaque</div>
+                        </div>
+                        <div class="imagemBarba-container">
+                            <img class="imagemBarba" src="img/barba1.jpeg" alt="Barba pigmentada">
+                            <div class="texto-hover">Barba pigmentada</div>
+                        </div>
+                        <div class="imagemBarba-container">
+                            <img class="imagemBarba" src="img/barba1.jpeg" alt="Barba com enfase no bigode">
+                            <div class="texto-hover">Barba com enfase no bigode</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="imagem_container">
+                    <div class="box_dread">
+                        <div class="imagemDread-container">
+                            <img class="imagemDread" src="img/dread1.png" alt="dread">
+                            <div class="texto-hover">dread</div>
+                        </div>
+                        <div class="imagemDread-container">
+                            <img class="imagemDread" src="img/dread1.png" alt="dread">
+                            <div class="texto-hover">dread</div>
+                        </div>
+                        <div class="imagemDread-container">
+                            <img class="imagemDread" src="img/dread1.png" alt="dread">
+                            <div class="texto-hover">dread</div>
+                        </div>
+                        <div class="imagemDread-container">
+                            <img class="imagemDread" src="img/dread1.png" alt="dread">
+                            <div class="texto-hover">dread</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="imagem_container">
+                    <div class="box_sobrancelha">
+                        <div class="imagemSobrancelha-container">
+                            <img class="imagemSobrancelha" src="img/sobrancelha1.jpeg"
+                                alt="Sobrancelha bem feita">
+                            <div class="texto-hover">Sobrancelha bem feita</div>
+                        </div>
+                        <div class="imagemSobrancelha-container">
+                            <img class="imagemSobrancelha" src="img/sobrancelha1.jpeg"
+                                alt="Sobrancelha bem feita">
+                            <div class="texto-hover">Sobrancelha bem feita</div>
+                        </div>
+                        <div class="imagemSobrancelha-container">
+                            <img class="imagemSobrancelha" src="img/sobrancelha1.jpeg"
+                                alt="Sobrancelha bem feita">
+                            <div class="texto-hover">Sobrancelha bem feita</div>
+                        </div>
+                        <div class="imagemSobrancelha-container">
+                            <img class="imagemSobrancelha" src="img/sobrancelha1.jpeg"
+                                alt="Sobrancelha bem feita">
+                            <div class="texto-hover">Sobrancelha bem feita</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="space"></div>
+        <h4>marque seu horário no nosso <span style="color: #7d1ecc;"> whatsapp
+            </span></h4>
+        <div class="cabelos_links">
+            <div class="side">
+                <button><a href="https://wa.me/5511970428582" target="_blank">whatsapp</a></button>
+            </div>
+        </div>
+    </article>
+</section>
