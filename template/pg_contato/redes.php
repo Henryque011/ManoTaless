ab0fb661dcfbd9a2db49a490fa532d2c02daefa3
og <henryquenonatosilva@gmail.com>
2025-02-07 16:56:56 -0300

    session redes.php

---
+++ template/pg_contato/redes.php
@@ -0,0 +1,12 @@
+<section class="redes">
+    <article class="site">
+        <div class="redes_sociais">
+            <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
+                    class="fa-brands fa-instagram"></i></a>
+            <a href="https://wa.me/5511970428582" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank"><i
+                    class="fa-brands fa-tiktok"></i></a>
+            <a href="#"><i class="fa-brands fa-spotify"></i></a>
+        </div>
+    </article>
+</section>
