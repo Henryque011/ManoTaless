e03584ed8ecb968c9448b4397a3c1538b3a86af5
og <henryquenonatosilva@gmail.com>
2025-02-07 15:21:37 -0300

    caurousel php

---
+++ template/carousel_sobre.php
@@ -0,0 +1,41 @@
+<section class="carousel_sobre">
+    <article class="site">
+        <h2>Assista alguns dos meus trabalhos</h2>
+
+        <div class="box_carousel">
+            <div>
+                <h2>trançista</h2>
+                <img src="img/tranca1.png" alt="Foto - de uma trança">
+            </div>
+            <div>
+                <h2>dançarino</h2>
+                <img src="img/danca.png" alt="Foto - dançando">
+            </div>
+            <div>
+                <h2>modelo</h2>
+                <img src="img/fotomissao.png" alt="Foto - de modelo">
+            </div>
+            <div>
+                <h2>dreadmaker</h2>
+                <img src="img/dread1.png" alt="Foto - fazedno dreads">
+            </div>
+            <div>
+                <h2>barbeiro</h2>
+                <img src="img/corte1.png" alt="Foto - corte de cabelo">
+            </div>
+            <div>
+                <h2>Cantor</h2>
+                <img src="img/corte3.png" alt="Foto - Cantando">
+            </div>
+        </div>
+        <div class="space">
+
+        </div>
+        <div class="side">
+            <button><a href="servicos.html">saiba mais sobre</a></button>
+            <button><a href="contato.html">entre em contato</a></button>
+        </div>
+        <div class="space">
+        </div>
+    </article>
+</section>
