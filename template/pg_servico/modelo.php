89cc26972dc1a2e63b0e8e24f2f5c6faba3aefa3
og <henryquenonatosilva@gmail.com>
2025-02-07 15:57:55 -0300

    moedelo.php

---
+++ template/pg_servico/modelo.php
@@ -0,0 +1,31 @@
+<section class="modelo" role="Sessão para descrever o trabalho como modelo">
+    <article class="site">
+        <h2>Meus trabalhos</h2>
+        <h3>modelo</h3>
+        <div class="side">
+            <div class="modelo_carac">
+                <h2>características fisicas:</h2>
+                <ul>
+                    <li>altura: 1,80m</li>
+                    <li>peso: 75kgs</li>
+                    <li>tipo de corpo: ectomorfo</li>
+                    <li>cor do cabelo: preto</li>
+                    <li>cor dos ohos: marrom</li>
+                    <li>tom de pele: negra</li>
+                    <li>sorriso alinhado e dentes brancos</li>
+                </ul>
+                <h2>medidas e projeção</h2>
+                <ul>
+                    <li>medidas: 88cm tronco - 75cm cintura - 70cm perna</li>
+                    <li>tamanho de tênis: 42(br)</li>
+                    <li>tamanho de roupas: g (grande)</li>
+                    <li>estilo: streetwer</li>
+                </ul>
+            </div>
+            <div>
+                <img src="img/fotomissao.png" alt="Foto de modelo">
+            </div>
+        </div>
+        <div class="space"></div>
+    </article>
+</section>
