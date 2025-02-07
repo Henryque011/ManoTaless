99e80a9ddbc7791f0c66fd585fdf9f1b82c0e9b0
og <henryquenonatosilva@gmail.com>
2025-02-07 15:57:32 -0300

    dancarino.php

---
+++ template/pg_servico/dancarino.php
@@ -0,0 +1,57 @@
+<section class="dancarino">
+    <div class="banner">
+    </div>
+    <article class="site">
+        <h2>Meus trabalhos</h2>
+        <h3>dançarino</h3>
+        <div class="side">
+            <div class="sobre_danca">
+                <h2>dançarino oficial da <span style="color: #7d1ecc;"> dj tha4ys </span></h2>
+                <p>Lorem ipsum dolor sit, amet consectetur <span style="color: #7d1ecc;"> adipisicing
+                    </span> Deserunt, corrupti
+                    repellendus omnis assumenda numquam nemo commodi mollitia ut iste laudantium
+                    consequatur odit aut reiciendis
+                    natus repellat recusandae! Tempora ipsam nisi, cupiditate totam eligendi non ad,
+                    quis <span style="color: #7d1ecc;"> voluptatum </span> fugit excepturi voluptas,
+                    ipsa voluptates. Voluptas expedita id ut
+                    expedita soluta
+                    dolorum ratione. Quasi ea, ipsa molestias <span style="color: #7d1ecc;"> aspernatur
+                        voluptatibus </span>
+                    soluta! Eos
+                    temporibus ratione amet ducimus vero, aliquam ad, ipsum aperia incidunt ea
+                    exercitationem consectetur reprehenderit ipsum. Recusandae veritatis vero aut
+                    itaque?</p>
+                <h2>eventos que participei</h2>
+                <div class="sobre_carrosel">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <h2>podparty</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>submundo 808</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>central 1926</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>lollapalooza</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>mega baile luxx</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>bail3 da th4ys</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="danca_mp4">
+                <video width="400px" controls>
+                    <source src="video/video1.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+    </article>
+</section>
