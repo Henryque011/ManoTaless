ff89582dc9c50d9bd0a609c1f97bd7fc161fe7da
og <henryquenonatosilva@gmail.com>
2025-02-07 15:21:21 -0300

    apresentation php

---
+++ template/apresentation.php
@@ -0,0 +1,56 @@
+<section class="presentation">
+    <article class="site">
+        <div class="side">
+            <div class="sobre">
+                <h2>eu sou o mano taless</h2>
+                <div class="sobre_carrosel">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <h2>Modelo</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>trançista</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>barbeiro</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>dançarino</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>dreadmaker</h2>
+                            </div>
+                            <div class="carousel-item">
+                                <h2>cantor</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="sobre_txt">
+                    <p>Lorem ipsum dolor sit, amet consectetur <span style="color: #7d1ecc;"> adipisicing
+                        </span> Deserunt, corrupti
+                        repellendus omnis assumenda numquam nemo commodi mollitia ut iste laudantium
+                        voluptates quasi natus ab aut in autem similique! Ut inventore facilis labore
+                        aliquam consequuntur eaque, quia cum laborum harum consequatur odit aut reiciendis
+                        natus repellat recusandae! Tempora ipsam nisi, cupiditate totam eligendi non ad,
+                        quis <span style="color: #7d1ecc;"> voluptatum </span> fugit excepturi voluptas,
+                        ipsa voluptates. Voluptas expedita id ut
+                        sunt facilis porro unde aut incidunt eius. Dolorum neque consequatur impedit
+                        necessitatibus laborum numquam quo blanditiis a omnis at totam vero expedita soluta
+                        corporis adipisci sit aspernatur, rerum delectus ab dolorem placeat nam. Doloremque,
+                        dolorum ratione. Quasi ea, ipsa molestias <span style="color: #7d1ecc;"> aspernatur
+                            voluptatibus </span>
+                        soluta! Eos
+                        temporibus ratione amet ducimus vero, aliquam ad, ipsum aperiam quaerat incidunt ea
+                        cupiditate sit sint iure consectetur ut optio, consequuntur facere. Porro
+                        exercitationem consectetur reprehenderit ipsum. Recusandae veritatis vero aut
+                        itaque?</p>
+                </div>
+            </div>
+            <div class="sobre_img">
+                <img src="img/tranca4.png" alt="Foto apresentativa sobre quem eu sou">
+            </div>
+        </div>
+    </article>
+</section>
