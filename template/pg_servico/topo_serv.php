4284d97927417e9f28e36b0b0e56821a643446d4
og <henryquenonatosilva@gmail.com>
2025-02-07 15:58:07 -0300

    topo_serv.php

---
+++ template/pg_servico/topo_serv.php
@@ -0,0 +1,51 @@
+<header>
+    <section class="site">
+        <div class="topo">
+            <div>
+                <a href="index.php"><img src="img/logo.svg" alt="Logo - Mano Taless"></a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a class="active" href="servicos.php">Serviços</a></li>
+                    <li><a href="contato.html">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="menu" id="menu-toggle">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+            <div class="menu-lateral" id="menu-lateral">
+                <button class="btn-sair"><i class="fa-solid fa-arrow-left"></i>Sair</button>
+                <ul>
+                    <li><img src="img/logo.svg" alt="Logo mano taless"></li>
+                    <li style="display: flex;justify-content: space-between;">
+                        <p>redes sociais:</p>
+                        <div class="redes_sociais" style="display: contents;">
+                            <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
+                                    class="fa-brands fa-instagram"></i></a>
+                            <a href="https://wa.me/5511970428582" target="_blank"><i
+                                    class="fa-brands fa-whatsapp"></i></a>
+                            <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank"><i
+                                    class="fa-brands fa-tiktok"></i></a>
+                            <a href="#"><i class="fa-brands fa-spotify"></i></a>
+                        </div>
+                    </li>
+                    <li>
+                        <p style="display: flex; justify-content: space-between;">
+                            telefone: <a href="https://wa.me/5511970428582">(11)970428582<i
+                                    class="fa-solid fa-phone"></i></a>
+                        </p>
+                    </li>
+                    <li>
+                        <p style="display: flex; justify-content: space-between;">
+                            endereço: <a
+                                href="https://www.google.com.br/maps/@-23.517101,-46.4223075,3a,75y,257.17h,90.77t/data=!3m7!1e1!3m5!1svHapGgESPOsFGsTwFr7eXg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-0.7708500411782353%26panoid%3DvHapGgESPOsFGsTwFr7eXg%26yaw%3D257.1718916908085!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D"
+                                target="_blank">av. nordestina n°4350<i class="fa-solid fa-location-dot"></i></a>
+                        </p>>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="space"></div>
+</header>
