236d6848481b93df880ce0e48c5393877f8d7cb0
Henryque Nonato Silva <henryquenonatosilva@gmail.com>
2025-02-08 23:45:54 -0300

    pg homr

--- template/videos_sobre.php
+++ template/videos_sobre.php
@@ -21,7 +21,7 @@
                 <h3>todo contato é feito exclusivamente pelo <span style="color: #7d1ecc;">whatsapp</span></h3>
                 <button><a href="contato.html">entre em contato</a></button>
             </div>
-            <div class="box_video">
+            <div class="box_video" style="width: 450px;">
                 <div><video width="100%" controls style="width: 325px; border-radius: 20px;">
                         <source src="video/video1.mp4" type="video/mp4">
                     </video>
@@ -30,18 +30,18 @@
                         <source src="video/video1.mp4" type="video/mp4">
                     </video>
                 </div>
-                <!-- <div><video width="100%" controls> 
+                <div><video width="100%" controls style="width: 325px; border-radius: 20px;">
                         <source src="video/video1.mp4" type="video/mp4">
                     </video>
                 </div>
-                <div><video width="100%" controls>
+                <div><video width="100%" controls style="width: 325px; border-radius: 20px;">
                         <source src="video/video1.mp4" type="video/mp4">
                     </video>
                 </div>
-                <div><video width="100%" controls>
+                <div><video width="100%" controls style="width: 325px; border-radius: 20px;">
                         <source src="video/video1.mp4" type="video/mp4">
                     </video>
-                </div> -->
+                </div>
             </div>
         </div>
     </article>
