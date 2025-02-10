5196bd4e30910a44367cb530595d3ff6ed0a636d
og <henryquenonatosilva@gmail.com>
2025-02-10 19:23:04 -0300

    REDDES  TOPO

--- template/pg_contato/topo_contato.php
+++ template/pg_contato/topo_contato.php
@@ -22,12 +22,11 @@
                         <p>redes sociais:</p>
                         <div class="redes_sociais" style="display: contents;">
                             <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
-                                    class="fa-brands fa-instagram"></i></a>
-                            <a href="https://wa.me/5511970428582" target="_blank"><i
-                                    class="fa-brands fa-whatsapp"></i></a>
-                            <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank"><i
-                                    class="fa-brands fa-tiktok"></i></a>
-                            <a href="#"><i class="fa-brands fa-spotify"></i></a>
+                                    class="fa-brands fa-instagram" title="INSTAGRAM"></i></a>
+                            <a href="https://wa.me/5511970428582" target="_blank"><i class="fa-brands fa-whatsapp" title="WHATSAPP"></i></a>
+                            <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank" title="TIKTOK"><i
+                                    class=" fa-brands fa-tiktok"></i></a>
+                            <a href="#"><i class="fa-brands fa-spotify" title="SPOTIFY"></i></a>
                         </div>
                     </li>
                     <li>
