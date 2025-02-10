d409e5532a5d09781af6ebbf2659e9ccc445e748
og <henryquenonatosilva@gmail.com>
2025-02-10 19:12:38 -0300

    TITLE REDES

--- template/footer.php
+++ template/footer.php
@@ -15,11 +15,11 @@
                 <h3>redes socias e localização</h3>
                 <div class="redes_sociais">
                     <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
-                            class="fa-brands fa-instagram"></i></a>
-                    <a href="https://wa.me/5511970428582" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
-                    <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank"><i
-                            class="fa-brands fa-tiktok"></i></a>
-                    <a href="#"><i class="fa-brands fa-spotify"></i></a>
+                            class="fa-brands fa-instagram" title="INSTAGRAM"></i></a>
+                    <a href="https://wa.me/5511970428582" target="_blank"><i class="fa-brands fa-whatsapp" title="WHATSAPP"></i></a>
+                    <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank" title="TIKTOK"><i
+                            class=" fa-brands fa-tiktok"></i></a>
+                    <a href="#"><i class="fa-brands fa-spotify" title="SPOTIFY"></i></a>
                 </div>
                 <a href="https://www.google.com.br/maps/@-23.517101,-46.4223075,3a,75y,257.17h,90.77t/data=!3m7!1e1!3m5!1svHapGgESPOsFGsTwFr7eXg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D-0.7708500411782353%26panoid%3DvHapGgESPOsFGsTwFr7eXg%26yaw%3D257.1718916908085!7i16384!8i8192?entry=ttu&g_ep=EgoyMDI1MDExMC4wIKXMDSoASAFQAw%3D%3D"
                     target="_blank">Avenida
