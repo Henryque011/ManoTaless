962c0d6c8f20d563a036f3b7b1aa72dd68288761
og <henryquenonatosilva@gmail.com>
2025-02-11 15:04:39 -0300

    videos updt

--- js/animation.js
+++ js/animation.js
@@ -12,15 +12,41 @@ $(document).ready(function () {
 
 // carrossel videos
 $(document).ready(function () {
-    $('.box_video').slick({
+    var $slider = $('.box_video');
+
+    $slider.slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true
+        dots: true,
+        autoplay: false
+    });
+
+    $slider.on('afterChange', function (event, slick, currentSlide) {
+        var $currentSlide = $(slick.$slides[currentSlide]);
+        var video = $currentSlide.find('video').get(0);
+        if (video) {
+            video.currentTime = 0;
+            video.play();
+        }
+    });
+
+    $slider.find('video').on('ended', function () {
+        $slider.slick('slickNext');
     });
 });
+
+
+// $(document).ready(function () {
+//     $('.box_video').slick({
+//         infinite: true,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//         dots: true
+//     });
+// });
 
 
 
