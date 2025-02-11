f698ecdf130ea00acc4488cf5cfe5613ab17caa0
og <henryquenonatosilva@gmail.com>
2025-02-11 18:49:30 -0300

    loader all pgs

--- servicos.php
+++ servicos.php
@@ -28,10 +28,7 @@
 </head>
 
 <body>
-    <!-- Overlay do Loader -->
-    <div id="loader-overlay" style="display: none;">
-        <div class="loader"></div>
-    </div>
+    <?php require_once('template/loader.php') ?>
     <?php require_once('template/pg_servico/topo_serv.php') ?>
     <main>
         <?php require_once('template/pg_servico/modelo.php') ?>
