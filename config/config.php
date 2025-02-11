89457b6529dfd4bb3454a3d86b73b8d6aca11032
og <henryquenonatosilva@gmail.com>
2025-02-11 18:07:47 -0300

    config

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+if(session_status() == PHP_SESSION_NONE){
+    session_start();
+}
+define("BASE_URL","http://localhost/manotaless/manotaless/");
+
+define('HOST_EMAIL','smtp.gmail.com');
+define('PORT_EMAIL','587');
+define('USER_EMAIL','abxqtzseven@gmail.com');
+define('PASS_EMAIL','mvll lewe mtxj ugeb');
