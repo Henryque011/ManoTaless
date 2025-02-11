2cf3af20ed64982fc26d08ef2100a0f5bc5d165b
og <henryquenonatosilva@gmail.com>
2025-02-11 20:37:11 -0300

    vlibras

--- template/topo.php
+++ template/topo.php
@@ -1,10 +1,12 @@
 <header>
-    <div vw class="enabled">
+    <div class="vlibras"> 
+        <div vw class="enabled">
             <div vw-access-button class="active"></div>
             <div vw-plugin-wrapper>
                 <div class="vw-plugin-top-wrapper"></div>
             </div>
         </div>
+    </div>
     <section class="site">
         <div class="topo">
             <div>
