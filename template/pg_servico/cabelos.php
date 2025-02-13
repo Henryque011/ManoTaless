6f1785b81c34996b4627e0a60293f0f4d47c3565
og <henryquenonatosilva@gmail.com>
2025-02-13 15:55:52 -0300

    nova estrutura pg servico

--- template/pg_servico/cabelos.php
+++ template/pg_servico/cabelos.php
@@ -2,44 +2,54 @@
     <article class="site">
         <h2>Meus trabalhos</h2>
         <h3>trançista - barbeiro - dreadmaker</h3>
-        <div class="side">
-            <div class="box_cabelo">
-                <div>
-                    <img src="img/corte1.png" alt="Corte de cabelo">
-                    <p>cortes a patir de 30 reais</p>
-                </div>
-                <div>
-                    <img src="img/sobrancelha1.jpeg" alt="sobrancelha">
-                    <p>sobrancelha a patir de 15 reais</p>
-                </div>
-                <div>
-                    <img src="img/dread1.png" alt="Dreads">
-                    <p>dreads a patir de 300 reais</p>
+        <div class="box_cabelo">
+            <div class="card" style="width: 18rem;">
+                <img src="img/corte1.png" class="card-img-top" alt="Corte de cabelo">
+                <div class="card-body">
+                    <p class="card-text">cortes a patir de 30 reais</p>
                 </div>
             </div>
-            <div class="box_cabelo">
-                <div>
-                    <p>tranças a patir de 100 reais</p>
-                    <img src="img/tranca1.png" alt="Tranças">
+            <div class="card" style="width: 18rem;">
+                <img src="img/sobrancelha1.jpeg" class="card-img-top" alt="sobrancelha">
+                <div class="card-body">
+                    <p class="card-text">sobrancelha a patir de 15 reais</p>
                 </div>
-                <div>
-                    <p>barba a patir de 15 reais</p>
-                    <img src="img/barba1.jpeg" alt="Barbas">
-                </div>
-                <div>
-                    <p>cortes apatir de R$30</p>
-                    <img src="img/corte3.png" alt="Cortes de cabelos">
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/dread1.png" class="card-img-top" alt="Dreads">
+                <div class="card-body">
+                    <p class="card-text">dreads a patir de 300 reais</p>
                 </div>
             </div>
         </div>
-        <h4>para marcar seu horario entre em contato diretamente pelo <span style="color: #7d1ecc;"> whatsapp
-            </span></h4>
-        <div class="cabelos_links">
-            <div class="side">
-                <button><a href="https://wa.me/5511970428582" target="_blank">whatsapp</a></button>
-                <button><a href="contato.html">redes sociais</a></button>
+        <div class="box_cabelo">
+            <div class="card" style="width: 18rem;">
+                <img src="img/tranca1.png" class="card-img-top" alt="Tranças">
+                <div class="card-body">
+                    <p class="card-text">tranças a patir de 100 reaiss</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/barba1.jpeg" class="card-img-top" alt="Barbas">
+                <div class="card-body">
+                    <p class="card-text">barba a patir de 15 reais</p>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="img/corte3.png" class="card-img-top" alt="Cortes de cabelos">
+                <div class="card-body">
+                    <p class="card-text">cortes apatir de R$30</p>
+                </div>
             </div>
         </div>
-        <div class="space"></div>
+        <!-- <h4>para marcar seu horario entre em contato diretamente pelo <span style="color: #7d1ecc;"> whatsapp
+                </span></h4>
+            <div class="cabelos_links">
+                <div class="side">
+                    <button><a href="https://wa.me/5511970428582" target="_blank">whatsapp</a></button>
+                    <button><a href="contato.html">redes sociais</a></button>
+                </div>
+            </div>
+            <div class="space"></div> --> -->
     </article>
 </section>
