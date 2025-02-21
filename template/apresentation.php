bf9478d3e5bd970ae117d0aba7a25866bf4af6b3
og <henryquenonatosilva@gmail.com>
2025-02-21 19:42:30 -0300

    Novo texto sobre

--- template/apresentation.php
+++ template/apresentation.php
@@ -28,7 +28,7 @@
                     </div>
                 </div>
                 <div class="sobre_txt">
-                    <p class="perfil">
+                    <p>
                         Tenho <span class="destaque">27 anos</span> e, ao longo de <span class="destaque">12 anos de experiência</span>, venho me aperfeiçoando no universo dos <span class="destaque">cortes de cabelo</span>. Nos últimos <span class="destaque">5 anos</span>, me especializei em <span class="destaque">tranças</span> e, no último ano, mergulhei no mundo dos <span class="destaque">dreads</span>, sempre buscando inovar e oferecer o melhor aos meus clientes.
                         <br>
                         Sou formado pela <span class="destaque">Universidade Zumbi dos Palmares</span>. A <span class="destaque">dança</span> sempre fez parte de minha vida desde a infância. Hoje, sou <span class="destaque">dançarino</span> da DJ <span class="destaque">Th4ys</span> e tenho participado de grandes <span class="destaque">eventos</span>. Além disso, também sou <span class="destaque">modelo</span> e <span class="destaque">cantor</span>, explorando diferentes formas de <span class="destaque">expressão artística</span> e sempre me reinventando em cada área.
