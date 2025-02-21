fb5bc3fa4d17091a87561171cc420920b98b27d4
og <henryquenonatosilva@gmail.com>
2025-02-21 20:15:35 -0300

    text dancarino

--- template/pg_servico/dancarino.php
+++ template/pg_servico/dancarino.php
@@ -6,21 +6,14 @@
         <h3>dançarino</h3>
         <div class="side">
             <div class="sobre_danca" style="width: 60%;">
-                <h2>dançarino oficial da <span style="color: #7d1ecc;"> dj tha4ys </span></h2>
-                <p>Lorem ipsum dolor sit, amet consectetur <span style="color: #7d1ecc;"> adipisicing
-                    </span> Deserunt, corrupti
-                    repellendus omnis assumenda numquam nemo commodi mollitia ut iste laudantium
-                    consequatur odit aut reiciendis
-                    natus repellat recusandae! Tempora ipsam nisi, cupiditate totam eligendi non ad,
-                    quis <span style="color: #7d1ecc;"> voluptatum </span> fugit excepturi voluptas,
-                    ipsa voluptates. Voluptas expedita id ut
-                    expedita soluta
-                    dolorum ratione. Quasi ea, ipsa molestias <span style="color: #7d1ecc;"> aspernatur
-                        voluptatibus </span>
-                    soluta! Eos
-                    temporibus ratione amet ducimus vero, aliquam ad, ipsum aperia incidunt ea
-                    exercitationem consectetur reprehenderit ipsum. Recusandae veritatis vero aut
-                    itaque?</p>
+                <h2>dançarino oficial da <span class="destaque""> dj tha4ys </span></h2>
+                <p>
+                    A dança sempre fez parte da minha vida, desde a infância. Desde pequeno, me sentia atraído pela liberdade de expressão e pelo ritmo que ela proporcionava. Ao longo dos anos, fui me aprofundando no <span class="destaque">funk</span>, um estilo que se tornou minha verdadeira paixão.
+                    <br>
+                    Nos últimos <span class="destaque">4 anos</span>, tive a oportunidade de dançar em grandes eventos de funk em São Paulo, ao lado da <span class="destaque">DJ Th4ys</span>, como no <span class="destaque">Submundo808</span> e na <span class="destaque">Podparty</span>, entre outros. Esses eventos me ajudaram a crescer como dançarino e a entender ainda mais a energia única dessa cultura.
+                    <br>
+                    Hoje, a dança não é só uma forma de expressão para mim, mas uma maneira de me conectar com o público e transmitir emoção. Cada apresentação é uma troca de energia que me motiva a continuar nessa jornada.
+                </p>
                 <h2>eventos que participei</h2>
                 <div class="sobre_carrosel">
                     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
