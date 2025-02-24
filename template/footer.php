5e67b8fbe8dd015d5c90f7801b4c476fd4e3c5cd
og <henryquenonatosilva@gmail.com>
2025-02-24 18:13:07 -0300

    link para o whatsapp

--- template/footer.php
+++ template/footer.php
@@ -16,7 +16,7 @@
                 <div class="redes_sociais">
                     <a href="https://www.instagram.com/mano_taless/" target="_blank"><i
                             class="fa-brands fa-instagram" title="INSTAGRAM"></i></a>
-                    <a href="https://wa.me/5511970428582" target="_blank"><i class="fa-brands fa-whatsapp" title="WHATSAPP"></i></a>
+                    <a href="https://w.app/mano_taless" target="_blank"><i class="fa-brands fa-whatsapp" title="WHATSAPP"></i></a>
                     <a href="https://www.tiktok.com/@mano.taless?lang=en" target="_blank" title="TIKTOK"><i
                             class=" fa-brands fa-tiktok"></i></a>
                     <a href="#"><i class="fa-brands fa-spotify" title="SPOTIFY"></i></a>
